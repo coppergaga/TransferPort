365ff49a47c7a991f1e3262b7e4d9406e8e4e9ca
copper <411998690@qq.com>
2026-01-16 14:52:49 +0800

    [update]deprecated unused class PortalUtilityNetworkLinkContainer

--- TransferPort/Common/PortalUtilityNetworkLinkContainer.cs
+++ TransferPort/Common/PortalUtilityNetworkLinkContainer.cs
@@ -1,32 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
-namespace RsTransferPort
-{
-    public class PortalUtilityNetworkLinkContainer
-    {
+namespace RsTransferPort {
+    [Obsolete("deprecated", true)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class PortalUtilityNetworkLinkContainer {
         private readonly Dictionary<int, Dictionary<string, HashSet<WirelessPowerPort>>> container;
 
-
-        public PortalUtilityNetworkLinkContainer()
-        {
+        public PortalUtilityNetworkLinkContainer() {
             App.OnPreLoadScene += Clear;
             container =
                 new Dictionary<int, Dictionary<string, HashSet<WirelessPowerPort>>>();
         }
 
-        public HashSet<WirelessPowerPort> Add(int worldId, string name,
-            WirelessPowerPort wirelessPowerPort)
-        {
-            Dictionary<string, HashSet<WirelessPowerPort>> nameContainer = null;
-            HashSet<WirelessPowerPort> portalWireSet = null;
-            if (!container.TryGetValue(worldId, out nameContainer))
-            {
+        public HashSet<WirelessPowerPort> Add(int worldId, string name, WirelessPowerPort wirelessPowerPort) {
+            if (!container.TryGetValue(worldId, out Dictionary<string, HashSet<WirelessPowerPort>> nameContainer)) {
                 nameContainer = new Dictionary<string, HashSet<WirelessPowerPort>>();
                 container.Add(worldId, nameContainer);
             }
 
-            if (!nameContainer.TryGetValue(name, out portalWireSet))
-            {
+            if (!nameContainer.TryGetValue(name, out HashSet<WirelessPowerPort> portalWireSet)) {
                 portalWireSet = new HashSet<WirelessPowerPort>();
                 nameContainer.Add(name, portalWireSet);
             }
@@ -35,51 +29,37 @@ namespace RsTransferPort
             return portalWireSet;
         }
 
-        public void Remove(int worldId, string name, WirelessPowerPort wirelessPowerPort)
-        {
-            Dictionary<string, HashSet<WirelessPowerPort>> nameContainer = null;
-            HashSet<WirelessPowerPort> portalWireSet = null;
-
-            if (container.TryGetValue(worldId, out nameContainer) && nameContainer.TryGetValue(name, out portalWireSet))
-            {
-                portalWireSet.Remove(wirelessPowerPort);
-                if (portalWireSet.Count == 0) nameContainer.Remove(name);
-            }
+        public void Remove(int worldId, string name, WirelessPowerPort wirelessPowerPort) {
+            if (!container.TryGetValue(worldId, out Dictionary<string, HashSet<WirelessPowerPort>> nameContainer)) { return; }
+            if (!nameContainer.TryGetValue(name, out HashSet<WirelessPowerPort> portalWireSet)) { return; }
+            portalWireSet.Remove(wirelessPowerPort);
+            if (portalWireSet.Count == 0) nameContainer.Remove(name);
         }
 
-        public bool Contains(int worldId, string name, WirelessPowerPort wirelessPowerPort)
-        {
+        public bool Contains(int worldId, string name, WirelessPowerPort wirelessPowerPort) {
             var portalWireSet = GetPortalWireSet(worldId, name);
 
             return portalWireSet?.Contains(wirelessPowerPort) ?? false;
         }
 
-        public HashSet<WirelessPowerPort> GetPortalWireSet(int worldId, string name)
-        {
-            Dictionary<string, HashSet<WirelessPowerPort>> nameContainer = null;
-            HashSet<WirelessPowerPort> portalWireSet = null;
-
-            if (container.TryGetValue(worldId, out nameContainer) && nameContainer.TryGetValue(name, out portalWireSet))
+        public HashSet<WirelessPowerPort> GetPortalWireSet(int worldId, string name) {
+            if (container.TryGetValue(worldId, out Dictionary<string, HashSet<WirelessPowerPort>> nameContainer)
+                && nameContainer.TryGetValue(name, out HashSet<WirelessPowerPort> portalWireSet))
                 return portalWireSet;
-
             return null;
         }
 
-        public ICollection<string> GetNamesByWorld(int worldId)
-        {
-            Dictionary<string, HashSet<WirelessPowerPort>> nameContainer = null;
-            if (container.TryGetValue(worldId, out nameContainer)) return nameContainer.Keys;
+        public ICollection<string> GetNamesByWorld(int worldId) {
+            if (container.TryGetValue(worldId, out Dictionary<string, HashSet<WirelessPowerPort>> nameContainer)) return nameContainer.Keys;
 
             return new List<string>();
         }
 
-        public int Count(int worldId, string name)
-        {
+        public int Count(int worldId, string name) {
             return GetPortalWireSet(worldId, name)?.Count ?? 0;
         }
 
-        public void Clear()
-        {
+        public void Clear() {
             container.Clear();
         }
     }
